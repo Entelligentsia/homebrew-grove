63c69b973e03c534ffd5455c880df2ce5efae2d2
Boni Gopalan <boni@entelligentsia.in>
2026-06-23 14:36:46 +0530

    release: v0.1.6

--- Formula/grove.rb
+++ Formula/grove.rb
@@ -19,22 +19,22 @@ class Grove < Formula
   on_macos do
     on_arm do
       url "#{BASE}/grove-aarch64-apple-darwin.tar.gz"
-      sha256 "REPLACE_AARCH64_APPLE_DARWIN"
+      sha256 "bfad852c475d930c607a09e28e387b6d747ccedcdfc411ae8804913ae3b2dd71"
     end
     on_intel do
       url "#{BASE}/grove-x86_64-apple-darwin.tar.gz"
-      sha256 "REPLACE_X86_64_APPLE_DARWIN"
+      sha256 "b6bfef147c7e0be9fe9db834cd9a56f3b01e294f3fe0432ef70914dcedef3514"
     end
   end
 
   on_linux do
     on_arm do
       url "#{BASE}/grove-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_AARCH64_UNKNOWN_LINUX_GNU"
+      sha256 "3f4c4b308c08b402bcc656b1e728626f68f2e86e222688ba9abdff9f7fa0d15a"
     end
     on_intel do
       url "#{BASE}/grove-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "REPLACE_X86_64_UNKNOWN_LINUX_GNU"
+      sha256 "95bcff34d145d59bd1f8406753b6be52194e4dacf1dc4a961c67acf1e7fec8c7"
     end
   end
 
